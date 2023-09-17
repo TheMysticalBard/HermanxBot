9d8863a9e2e83ffa85aa972cf711426a96a6700d
psmyrl <parker.smyrl@gmail.com>
2023-09-17 14:45:31 -0500

    Add logging, update gitignore

--- hermanx_bot.py
+++ hermanx_bot.py
@@ -1,12 +1,16 @@
 import discord
+import logging
 import json
+
+log_level = logging.INFO
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
 
 with open("./private.json") as private:
     PRIVATE = json.load(private)
 
 intents = discord.Intents.default()
 intents.message_content = True
-
+intents.members = True
 client = discord.Client(intents=intents)
 
 @client.event
@@ -21,4 +25,4 @@ async def on_message(message: discord.Message):
     if message.content.startswith('ping'):
         await message.channel.send("pong")
 
-client.run(PRIVATE["discord_key"])
+client.run(PRIVATE["discord_key"], log_handler=handler, log_level=log_level)
